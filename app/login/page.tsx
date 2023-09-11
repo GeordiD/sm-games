a3c3a5d9ce6baef352152cdaa10821b63f791af6
GeordiD <geordi.dosher@gmail.com>
2023-09-11 08:34:35 -0500

    Remove firebase

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,40 +1,15 @@
 'use client'
 
-import { auth, provider } from '@/lib/firebase-config';
-import { getRedirectResult, signInWithPopup, signInWithRedirect } from 'firebase/auth';
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-
 export default function Login() {
-  const router = useRouter();
-
-  useEffect(() => {
-    getRedirectResult(auth).then(async (userCred) => {
-      if (!userCred) {
-        return;
-      }
-
-      fetch("/api/login", {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${await userCred.user.getIdToken()}`,
-        },
-      }).then((response) => {
-        if (response.status === 200) {
-          router.push("/poker");
-        }
-      });
-    });
-  }, []);
+  // const router = useRouter();
 
   function handleLogin() {
-    signInWithRedirect(auth, provider);
+    console.log('click');
   }
 
   return (
     <main className="flex min-h-screen justify-center items-center">
-      {/* <LoginForm /> */}
-      <button onClick={() => handleLogin()}>Log In</button>
+      <button onClick={() => handleLogin()}>Log In With Google</button>
     </main>
   )
 }
